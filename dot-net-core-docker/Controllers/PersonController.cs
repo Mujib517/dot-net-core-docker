04d2939ae3d54ee1c4beab623c32cc030afacb4e
Rahman <mr185242@ncr.com>
2017-10-23 19:19:07 +0530

    Sql Server Test

---
+++ dot-net-core-docker/Controllers/PersonController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace dot_net_core_docker.Controllers
+{
+    [Route("api/[controller]")]
+    public class PersonController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+
+            SqlConnection con = new SqlConnection("Data Source=WINMR185242-2AK;Initial Catalog=Person;Integrated Security=True");
+            con.Open();
+            return new string[] { "value1", "value2" };
+        }
+    }
+}
